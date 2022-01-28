4e5fe92463429984d27061df3f33618c20731b21
trisha112 <tagra002@ucr.edu>
2022-01-28 11:53:32 -0800

    fix tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -3,56 +3,56 @@
 
 TEST(Constructors, positiveVal){
     Rectangle obj1(6,8);
-    EXPECT_EQ(width == 6);
-    EXPECT_EQ(height == 8);
+    EXPECT_TRUE(obj1.width == 6);
+    EXPECT_TRUE(obj1.height == 8);
 }
 
 TEST(Constructors, oneNegativeVal){
     Rectangle obj2(-6, 8);
-    EXPECT_EQ(width == -6);
-    EXPECT_EQ(length == 8);
+    EXPECT_TRUE(obj2.width == -6);
+    EXPECT_TRUE(obj2.height == 8);
 }
 
 TEST(Constructors, twoNegativeVals){
     Rectangle obj3(-6, -8);
-    EXPECT_EQ(width == -6);
-    EXPECT_EQ(length == -8);
+    EXPECT_TRUE(obj3.width == -6);
+    EXPECT_TRUE(obj3.height == -8);
 }
 
 TEST(Area, positiveVal){
    Rectangle obj4(4,5);
    int obj4Area = obj4.area();
-   EXPECT_EQ(obj4Area == 20);
+   EXPECT_TRUE(obj4Area == 20);
 }
 
 TEST(Area, oneNegativeVal){
    Rectangle obj5(-4, 5);
    int obj5Area = obj5.area();
-   EXPECT_EQ(obj5Area == -20);
+   EXPECT_TRUE(obj5Area == -20);
 }
 
 TEST(Area, twoNegativeVals){
    Rectangle obj6(-4, -5);
    int obj6Area = obj6.area();
-   EXPECT_EQ(obj6Area == 20);
+   EXPECT_TRUE(obj6Area == 20);
 }
 
 TEST(Perimeter, positiveVal){
    Rectangle obj7(2,3);
-   int obj7Perimeter = obj7.perimter();
-   EXPECT_EQ(obj7Perimeter == 10);
+   int obj7Perimeter = obj7.perimeter();
+   EXPECT_TRUE(obj7Perimeter == 10);
 }
 
 TEST(Perimeter, oneNegativeVal){
    Rectangle obj8(-2,3);
    int obj8Perimeter = obj8.perimeter();
-   EXPECT_EQ(obj8Perimeter == 10);
+   EXPECT_TRUE(obj8Perimeter == 10);
 }
 
-TEST(Perimter, twoNegativeVals){
+TEST(Perimeter, twoNegativeVals){
    Rectangle obj9(-2, -3);
-   int obj8Perimeter = obj9.perimeter();
-   EXPECT_EQ(obj9Perimter == 10);
+   int obj9Perimeter = obj9.perimeter();
+   EXPECT_TRUE(obj9Perimeter == 10);
 }
 
 int main(int argc, char **argv) {
